5c7c7e44bfe08cc5ff405643873438cefe7bf7ce
Matheus Oliveira <matheus.hortolandia@gmail.com>
2022-10-01 00:52:37 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfoIOException
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string sourcePath = @"D:\Users\Matheus\Desktop\Curso - C# - Udemy\Projetos\file1.txt";
+            string targetPath = @"D:\Users\Matheus\Desktop\Curso - C# - Udemy\Projetos\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
